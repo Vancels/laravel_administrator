a91087e25f3e8430152ea15ae839710af5de5e49
Mr.Wang <281762391@qq.com>
2016-10-15 22:35:48 +0800

    add Test 1

---
+++ src/Vancels/Administrator/AdministratorServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace Vancels\Administrator;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdministratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+}

0d42e53df58525dbc92b4318b571f4f8e702619b
Mr.Wang <281762391@qq.com>
2016-10-15 22:32:14 +0800

    add Test 1

---
+++ src/controllers/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Vancels\Administrator;
+
+use Illuminate\Http\Exception\HttpResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\File;
+use Illuminate\Session\SessionManager as Session;
+use Symfony\Component\HttpFoundation\File\File as SFile;
+
+class AdminController extends Controller
+{
+
+
+}

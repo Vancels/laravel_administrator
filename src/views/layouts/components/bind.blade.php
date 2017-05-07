7a87aef25dd202bdb84d128689c04fb67f4506d5
Mr.Wang <281762391@qq.com>
2017-05-07 13:07:52 +0800

    锁定左侧导航

--- src/views/layouts/components/bind.blade.php
+++ src/views/layouts/components/bind.blade.php
@@ -4,4 +4,12 @@
 {!! html::script('admin_static/js/plugins/jeditable/jquery.jeditable.js') !!}
 <script>
     seajs.use('admin/bind_quick_status_edit');
+
+    // 锁定左侧
+    var body = $('body');
+    body.addClass('fixed-sidebar');
+    $('.sidebar-collapse').slimScroll({
+        height: '100%',
+        railOpacity: 0.9
+    });
 </script>
